ef41af50b36d621a5861a57cff469ca72cff9025
username40 <mr.v.e.romantsov@gmail.com>
2022-03-15 21:21:12 +0300

    start configuring webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+
+    ]
+}
