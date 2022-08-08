8733025dcdea5d87e772e66e623e0919e4ea699c
username40 <mr.v.e.romantsov@gmail.com>
2022-08-08 15:40:47 +0300

    write html for resizing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,8 +3,14 @@ import {createTable} from '@/components/table/table.template';
 // eslint-disable-next-line require-jsdoc
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
   // eslint-disable-next-line require-jsdoc
   toHTML() {
-    return createTable();
+    return createTable(20);
   }
 }
