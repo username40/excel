0f4872e95e0933050269d215c40725fb3d66c7b2
username40 <mr.v.e.romantsov@gmail.com>
2022-08-14 19:28:34 +0300

    resize cols

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComp';
 import {createTable} from '@/components/table/table.template';
-import {$} from "@core/dom";
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -20,11 +20,13 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target)
       const $parent = $resizer.el.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
-      console.log($parent.getCoords())
+      console.log($parent.data)
       document.onmousemove = e => {
         const delta = e.pageX - coords.right
         const value = coords.width + delta
         $parent.$el.style.width = `${value}px`
+        document.querySelectorAll(`[data-col="${parent.data.col}"]`)
+            .forEach(el => el.style.width = value + 'px')
       }
       document.onmouseup = () => {
         document.onmousemove = null
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,12 +3,12 @@ const CODES = {
   Z: 90
 };
 
-function toCell() {
-  return `<div class="cell" contenteditable></div>`
+function toCell(_, col) {
+  return `<div class="cell" contenteditable data-col="${col}"></div>`
 }
 
-function toColumn(col) {
-  return `<div class="column" data-type="resizable">
+function toColumn(col, index) {
+  return `<div class="column" data-type="resizable" data-col="${index}">
             ${col}
             <div class="col-resize" data-resize="col"></div>
         </div>`
