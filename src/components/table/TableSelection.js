8321a247599d3015d60194e775fc5ee3e0f63f96
slava <mr.v.e.romantsov@gmail.com>
2023-04-30 00:14:52 +0300

    make multiselect cells

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -2,12 +2,14 @@ export class TableSelection {
   static className = 'selected'
   constructor() {
     this.group = []
+    this.current = null
   }
   // $el instanceof DOM
   select($el) {
     this.clear()
-    this.group.push($el)
     $el.addClass(TableSelection.className)
+    this.group.push($el)
+    this.current = $el
   }
 
   clear() {
@@ -15,6 +17,9 @@ export class TableSelection {
     this.group = []
   }
 
-  selectGroup() {
+  selectGroup($group = []) {
+    this.clear()
+    this.group = $group
+    this.group.forEach($el => $el.addClass(TableSelection.className))
   }
 }
