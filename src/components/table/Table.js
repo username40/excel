684021525ee0998131ad5de33790d521b7e360a2
slava <mr.v.e.romantsov@gmail.com>
2023-05-22 21:49:45 +0300

    2 way binding

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,7 +16,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options
     })
   }
@@ -32,12 +32,20 @@ export class Table extends ExcelComponent {
   init() {
     super.init();
 
-    const $cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select($cell)
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
 
-    this.$on('formula: input', text => {
+    this.$on('formula:input', text => {
       this.selection.current.text(text)
     })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
   }
 
   onMousedown(event) {
@@ -69,8 +77,11 @@ export class Table extends ExcelComponent {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
+      this.selectCell($next)
     }
+  }
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
 
