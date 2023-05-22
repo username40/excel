97b137b71d9b97db0204759ddaa0da929b4e686e
slava <mr.v.e.romantsov@gmail.com>
2023-05-22 21:53:37 +0300

    little fix for chaining

--- src/core/dom.js
+++ src/core/dom.js
@@ -95,9 +95,11 @@ class Dom {
   }
   addClass(className) {
     this.$el.classList.add(className)
+    return this
   }
   removeClass(className) {
     this.$el.classList.remove(className)
+    return this
   }
 }
 
