9b7a78944ad1445e98e25dc91789140c9e33a9f3
slava <mr.v.e.romantsov@gmail.com>
2023-05-24 09:06:24 +0300

    continue make redux

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -41,11 +41,16 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus()
     })
+
+    this.$subscribe(state => {
+      console.log('TableState', state)
+    })
   }
 
   selectCell($cell) {
     this.selection.select($cell)
     this.$emit('table:select', $cell)
+    this.$dispatch({type: 'TEST'})
   }
 
   onMousedown(event) {
@@ -58,7 +63,7 @@ export class Table extends ExcelComponent {
             .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
-        this.selection.select($target)
+        this.selectCell($target)
       }
     }
   }
