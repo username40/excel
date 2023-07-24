34356329b64f35d5cdae9c43d4913b7937993722
slava <mr.v.e.romantsov@gmail.com>
2023-07-24 22:15:41 +0300

    finish table logic and state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -11,6 +11,7 @@ import {TableSelection} from '@/components/table/TableSelection';
 import {$} from '@core/dom';
 import * as actions from '@/redux/actions'
 import {defaultStyles} from '@/constants';
+import {parse} from '@core/parse';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -36,9 +37,10 @@ export class Table extends ExcelComponent {
 
     this.selectCell(this.$root.find('[data-id="0:0"]'))
 
-    this.$on('formula:input', text => {
-      this.selection.current.text(text)
-      this.updateTextInStore(text)
+    this.$on('formula:input', value => {
+      this.selection.current.attr('data-value', value)
+      this.selection.current.text(parse(value))
+      this.updateTextInStore(value)
     })
 
     this.$on('formula:done', () => {
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,5 +1,6 @@
 import {toInlineStyles} from '@core/utils';
 import {defaultStyles} from '@/constants';
+import {parse} from '@core/parse';
 
 const CODES = {
   A: 65,
@@ -32,8 +33,9 @@ function toCell(state, row) {
       contenteditable
       data-col="${col}"
       data-type="cell"
+      data-value="${data || ''}"
       style="${styles}; width: ${width}"
-      data-id="${id}">${data || ''}</div>
+      data-id="${id}">${parse(data) || ''}</div>
    `
   }
 }
